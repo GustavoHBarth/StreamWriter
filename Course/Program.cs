2ce9112ea586528bc9ce41bdbacf1b8e7d315a4e
Gustavo Barth <barthgustavo5@gmail.com>
2024-06-11 21:20:13 -0300

    Adicionar arquivos de projeto.

---
+++ Course/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcepath = @"c:\temp\file1.txt";
+            string targetpath = @"c:\temp\file2.txt";
+
+            try
+            {
+                //Ler todo conteudo e guardar no vetor de arquivos
+                string[] lines = File.ReadAllLines(sourcepath);
+
+                //Transformar tudo em letra maiscula
+                using (StreamWriter sw = File.AppendText(targetpath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
